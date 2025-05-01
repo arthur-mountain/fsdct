8df6da1681754960c0606aa4c67f0a69d0c32561
arthur <arthur.oceanus@gmail.com>
2025-05-01 22:06:33 +0800

    feat: load env for build docker-compose yaml

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/usr/local/bin bash
 
+set -a
+source ./client/.env
+source ./server/.env
+set +a
+
 docker-componse \
   -f docker-componse.network.yml \
   -f ./server/docker/docker-compose.yml \
