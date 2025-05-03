00a7395f7ca72f5172f9cd063823122f3bc18e4f
arthur <arthur.oceanus@gmail.com>
2025-05-03 17:48:44 +0800

    feat: enalbe redis cluster replicas...

--- server/src/model/redis/index.js
+++ server/src/model/redis/index.js
@@ -17,6 +17,15 @@ const getInstance = async (type) => {
           {
             url: `redis://${process.env.REDIS_HOST3}:${process.env.REDIS_PORT3}`,
           },
+          {
+            url: `redis://${process.env.REDIS_HOST4}:${process.env.REDIS_PORT4}`,
+          },
+          {
+            url: `redis://${process.env.REDIS_HOST5}:${process.env.REDIS_PORT5}`,
+          },
+          {
+            url: `redis://${process.env.REDIS_HOST6}:${process.env.REDIS_PORT6}`,
+          },
         ],
         defaults: {
           password: process.env.REDIS_PASSWORD,
